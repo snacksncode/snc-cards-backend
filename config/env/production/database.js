a2613ae3ac95fc515b57c564aef29c63891a6f37
snacksncode <snacksncode@gmail.com>
2021-10-22 13:26:04 +0200

    DB config

---
+++ config/env/production/database.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
